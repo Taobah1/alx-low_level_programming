f3ce824644202a7528ba5c72b39a3e037461d967
Taobah1 <ama@gmail.com>
2023-02-16 12:07:36 -0800

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line without printf but puts but puts function
+ * main - A program that prints a line without printf but puts function
  * Return: 0 (success)
  */
 int main(void)
