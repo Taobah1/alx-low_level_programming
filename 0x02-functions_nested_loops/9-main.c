4a854e3cc9c6dce1a94fc73dfc465e25a11f79a4
Taobah1 <ama@gmail.com>
2023-02-22 14:24:31 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -2,12 +2,11 @@
 
 /**
  * main - check the code
- * Return: Always 0
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-        print_alphabet();
-        return (0);
+    times_table();
+    return (0);
 }
-
